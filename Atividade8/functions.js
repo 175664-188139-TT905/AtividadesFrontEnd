9c2bd33d7ef3c655165eb3686e0ef274a9db764f
JaqueRohvedder <80848851+JaqueRohvedder@users.noreply.github.com>
2021-06-09 21:45:33 -0300

    all

--- Atividade8/functions.js
+++ Atividade8/functions.js
@@ -1,4 +1,4 @@
-let url = "https://tt905-175664-188139.herokuapp.com/gameslib/"
+let url = "https://tt905-175664-188139.herokuapp.com/gameslib"
 
 async function callFetchWithGet(){
     let headers = new Headers();
